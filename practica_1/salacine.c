7dc793b4da86918aa60b4f7ed4f3f6cae50e0de7
cid <zs24013120@estudiantes.uv.mx>
2026-03-26 17:36:58 -0600

    versión 1.0.1 Salacine.c
    Alfredo Cid Garcia
    fecha:26/03/2026
    se agrego sala++ para evitar un bucle infinito

--- practica_1/salacine.c
+++ practica_1/salacine.c
@@ -1,19 +1,29 @@
 #include <stdio.h>
+//Programa sala de cine
+//autor: Santiago
+// modificador: Alfredo
+// versión 1.0.1
+// se agrego sala++ para evitar un bucle infinito
 int main(){
-    int multiarreglo[5][4]={{ 1,2,3,4},{5,6,7,8},{9,10,11,12 },{13,14,15,16},{17,18,19,20}};
-    int fila,columna,sala=0;
-
-    while(sala<20){
-for(int x=0;x<5;x++){
-        for(int y=0;y<4;y++){
-            printf(" %i",multiarreglo[x][y]);
+    int multiarreglo[5][4]={{1,2,3,4},{5,6,7,8},{9,10,11,12},{13,14,15,16},{17,18,19,20}};
+    int fila, columna, sala=0;
+    int x,y;
+    while(sala < 20){
+        for( x=0; x<5; x++){
+            for(y=0; y<4; y++){
+                printf(" %i", multiarreglo[x][y]);
+            }
+            printf("\n");
         }
-        printf("\n");
+        printf("Escoje el asiento (Fila 1-5, Columna 1-4): \n");
+        scanf("%i %i", &fila, &columna);
+        fila = fila - 1; 
+        columna = columna - 1;
+        if (fila >= 0 && fila < 5 && columna >= 0 && columna < 4) {
+            multiarreglo[fila][columna] = 0;
+            sala++; 
+        } else {
+            printf("Error: Coordenadas invalidas.\n");
+        }
     }
-    printf("escoje el asiento\n");
-    printf("escribe la fila y columna\n");
-    scanf("%i %i",&fila,&columna);
-    multiarreglo[fila][columna]=0;
-
-}
 }
