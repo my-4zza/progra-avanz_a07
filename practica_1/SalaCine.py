fc3d153ef23900a8aca14f1fcb0f58a97f056143
cid <zs24013120@estudiantes.uv.mx>
2026-03-26 16:14:42 -0600

    versión 1.0.1
    Alfredo Cid Garcia 
    se agrego sala++ para evitar un bucle infinito.
    fecha:26/03/2026

--- practica_1/SalaCine.py
+++ practica_1/SalaCine.py
@@ -1,31 +1,29 @@
-def principal():
-
-    multiarreglo = [
-        [1, 2, 3, 4],
-        [5, 6, 7, 8],
-        [9, 10, 11, 12],
-        [13, 14, 15, 16],
-        [17, 18, 19, 20]
-    ]
-    
-    sala = 0
-    
-    while sala < 20:
-
-        for incognita in range(5):
-            for y in range(4):
-
-                print(f" {multiarreglo[incognita][y]}", end="")
- 
-            print("\n")
-        
-        print("escoje el asiento")
-        print("escribe la fila y columna")
-        
-        fila = int(input("Fila: "))
-        columna = int(input("Columna: "))
-        
-        multiarreglo[fila][columna] = 0
-
-if __name__ == "__main__":
-    principal()
+#include <stdio.h>
+//Programa sala de cine
+//autor: Santiago
+// modificador: Alfredo
+// versión 1.0.1
+// se agrego sala++ para evitar un bucle infinito
+int main(){
+    int multiarreglo[5][4]={{1,2,3,4},{5,6,7,8},{9,10,11,12},{13,14,15,16},{17,18,19,20}};
+    int fila, columna, sala=0;
+    int x,y;
+    while(sala < 20){
+        for( x=0; x<5; x++){
+            for(y=0; y<4; y++){
+                printf(" %i", multiarreglo[x][y]);
+            }
+            printf("\n");
+        }
+        printf("Escoje el asiento (Fila 1-5, Columna 1-4): \n");
+        scanf("%i %i", &fila, &columna);
+        fila = fila - 1; 
+        columna = columna - 1;
+        if (fila >= 0 && fila < 5 && columna >= 0 && columna < 4) {
+            multiarreglo[fila][columna] = 0;
+            sala++; 
+        } else {
+            printf("Error: Coordenadas invalidas.\n");
+        }
+    }
+}
