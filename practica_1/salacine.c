5c306987e0c89e64b524b1579377cab98ec490e4
jimmy840.111 <zs24013200@estudiantes.uv.mx>
2026-03-26 18:18:25 -0600

    Versión 1.1.1 salacine
    Jimmy Loucioss Alarcón Galván 
    26/03/2026
    Corrección de la falla en la introducción de datos, ya que si se ponía menos  o mas de los solicitados el programa se cerraba o no se ejecutaba

--- practica_1/salacine.c
+++ practica_1/salacine.c
@@ -1,40 +1,65 @@
 //Programa sala de cine
 //autor: Santiago
-// modificador: Antonio
-// versión 1.1.0
-// Se realizo algunas mejoras al codigo base.
-// Evita que un asiento que ya vale "0" y el feddback avisa si la reserva fue exitosa o fallida.
+// modificador: Jimmmy
+// versión 1.1.1
+// Corrección de la falla en la introducción de datos, ya que si se ponía menos  o mas de los solicitados el programa se cerraba no se ejecutaba o fallaba
 
 #include <stdio.h>
-int main(){
-int multiarreglo[5][4]={{1,2,3,4},{5,6,7,8},{9,10,11,12},{13,14,15,16},{17,18,19,20}};
-int fila, columna, sala=0;
 
-while(sala < 20){
-for(int x=0; x<5; x++){
-for(int y=0; y<4; y++){
-//Mejora aplicada: un formato mucho mas limpio
-printf("[%2i] ", multiarreglo[x][y]);
-}
-printf("\n");
-}
+int main() {
+    int multiarreglo[5][4] = {
+        {1, 2, 3, 4},
+        {5, 6, 7, 8},
+        {9, 10, 11, 12},
+        {13, 14, 15, 16},
+        {17, 18, 19, 20}
+    };
 
-printf("Ingresa fila y columna: ");
-scanf("%i %i", &fila, &columna);
+    int sala = 0; 
+    int fila, columna;
 
-fila--; columna--; // Ajuste de los indices
+    while (sala < 20) {
+        printf("\n--- ESTADO DE LA SALA ---\n");
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 4; j++) {
+                printf(" %2d ", multiarreglo[i][j]);
+            }
+            printf("\n");
+        }
 
-if (fila >= 0 && fila < 5 && columna >= 0 && columna < 4) {
-// Mejora aplicada: Verificacion si ya esta ocupado
-if(multiarreglo[fila][columna] == 0) {
-printf("error: el asiento ya esta ocupado.\n");
-} else {
-printf("reserva exitosa.\n");
-multiarreglo[fila][columna] = 0;
-sala++;
-}
-} else {
-printf("error: Fuera de rango.\n");
-}
-}
+        printf("\nEscoge el asiento\n");
+        printf("Escribe la fila (0-4) y columna (0-3)\n");
+
+        printf("Fila: ");
+       
+        if (scanf("%d", &fila) != 1) {
+            printf("Error: Por favor, ingresa solo números enteros.\n");
+            while(getchar() != '\n'); 
+            continue;
+        }
+
+        printf("Columna: ");
+        if (scanf("%d", &columna) != 1) {
+            printf("Error: Por favor, ingresa solo números enteros.\n");
+            while(getchar() != '\n');
+            continue;
+        }
+
+  
+        if (fila >= 0 && fila < 5 && columna >= 0 && columna < 4) {
+           
+            if (multiarreglo[fila][columna] == 0) {
+                printf("¡Ese asiento ya está ocupado! Por favor elige otro.\n");
+            } else {
+                multiarreglo[fila][columna] = 0;
+                sala++;
+                printf("¡Asiento reservado con éxito!\n");
+            }
+        } else {
+            printf("Error: Fila o columna fuera de los límites.\n");
+        }
+    }
+
+    printf("\n¡La sala está completamente llena!\n");
+    return 0;
 }
